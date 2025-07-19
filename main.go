3ca4942f9b4db05a7b255c3da2900984f1a94e84
Nathnael Desta <destanathnaelataro@gmail.com>
2025-07-19 13:58:57 +0300

    feat: add admin route

--- main.go
+++ main.go
@@ -11,44 +11,50 @@ type apiConfig struct {
 }
 
 func (cfg *apiConfig) returnHits(w http.ResponseWriter, r *http.Request) {
-	 fmt.Fprintf(w,"Hits: %v\n", cfg.fileserverHits.Load())
+	html := fmt.Sprintf(`
+	 <html>
+  		<body>
+    		<h1>Welcome, Chirpy Admin</h1>
+    		<p>Chirpy has been visited %d times!</p>
+  		</body>
+	</html>`, cfg.fileserverHits.Load())
+	w.Header().Set("Content-type", "text/html;")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(html))
 }
 
 func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
-	 cfg.fileserverHits.Swap(0)
+	cfg.fileserverHits.Swap(0)
 }
 
 func (cfg *apiConfig) middlewareMetricsIncrease(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r * http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
 
-		next.ServeHTTP(w,r)
+		next.ServeHTTP(w, r)
 	})
 }
-
-
-
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 	myApiConfig := apiConfig{
 		fileserverHits: atomic.Int32{},
+		
 	}
-
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", http.StripPrefix("/app/", myApiConfig.middlewareMetricsIncrease(http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("/healthz", func (w http.ResponseWriter,r *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
-	mux.HandleFunc("/metrics", myApiConfig.returnHits)
-	mux.HandleFunc("/reset", myApiConfig.reset)
+	mux.HandleFunc("GET /admin/metrics", myApiConfig.returnHits)
+	mux.HandleFunc("POST /admin/reset", myApiConfig.reset)
 
 	myServer := http.Server{
-		Addr: ":" + port,
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 	myServer.ListenAndServe()
